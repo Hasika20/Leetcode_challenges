4cf930d47410c8301c24422b4bded9534a1d27de
Hasika <hasika20.2005@gmail.com>
2024-03-17 14:44:02 +0530

    Binary Subarrays With Sum

---
+++ Binary Subarrays With Sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+
+       int count=0;
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            for(int j=i;j<nums.length;j++){
+                sum+=nums[j];
+                if(sum==goal){
+                    count++;
+                }
+                else if(sum>goal){
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
