4145613e051593369ca642562053a0dfc99a8dd6
Hasika <hasika20.2005@gmail.com>
2024-03-17 14:41:45 +0530

    Find the Pivot Integer

---
+++ Find the Pivot Integer.py
@@ -0,0 +1,14 @@
+from math import sqrt
+class Solution(object):
+    def pivotInteger(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        x = sqrt(n * (n + 1) / 2)
+        
+        if x % 1 != 0:
+            return -1
+        else:
+            return int(x)
+        
